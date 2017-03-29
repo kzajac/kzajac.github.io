5f3920ba762c0cdaa11b138f4ece3a33f3a52232
Michal Idzik <miidzik@agh.edu.pl>
2017-03-29 23:10:55 +0200

    graph.jl comments

--- julia/lab2/graphs.jl
+++ julia/lab2/graphs.jl
@@ -9,26 +9,34 @@ export GraphVertex, NodeType, Person, Address,
        graph_to_str, node_to_str,
        test_graph
 
+#= Single graph vertex type.
+Holds node value and information about adjacent vertices =#
 type GraphVertex
   value
   neighbors ::Vector
 end
 
+# Types of valid graph node's values.
 abstract NodeType
 
 type Person <: NodeType
   name
 end
 
-
 type Address <: NodeType
   streetNumber
 end
 
 
+# Number of graph nodes.
 N = 800
+
+# Number of graph edges.
 K = 10000
 
+
+#= Generates random directed graph of size N with K edges
+and returns its adjacency matrix.=#
 function generate_random_graph()
     A = Array{Int64,2}(N, N)
 
@@ -42,14 +50,17 @@ function generate_random_graph()
     A
 end
 
+# Generates random person object (with random name).
 function get_random_person()
   Person(randstring())
 end
 
+# Generates random person object (with random name).
 function get_random_address()
   Address(rand(1:100))
 end
 
+# Generates N random nodes (of random NodeType).
 function generate_random_nodes()
   nodes = Vector()
   for i= 1:N
@@ -58,6 +69,8 @@ function generate_random_nodes()
   nodes
 end
 
+#= Converts given adjacency matrix (NxN)
+  into list of graph vertices (of type GraphVertex and length N). =#
 function convert_to_graph(A, nodes)
   N = length(nodes)
   push!(graph, map(n -> GraphVertex(n, GraphVertex[]), nodes)...)
@@ -69,6 +82,8 @@ function convert_to_graph(A, nodes)
   end
 end
 
+#= Groups graph nodes into connected parts. E.g. if entire graph is connected,
+  result list will contain only one part with all nodes. =#
 function partition()
   parts = []
   remaining = Set(graph)
@@ -82,6 +97,9 @@ function partition()
   parts
 end
 
+#= Performs BFS traversal on the graph and returns list of visited nodes.
+  Optionally, BFS can initialized with set of skipped and remaining nodes.
+  Start nodes is taken from the set of remaining elements. =#
 function bfs(;visited=Set(), remaining=Set(graph))
   first = next(remaining, start(remaining))[1]
   q = [first]
@@ -104,6 +122,8 @@ function bfs(;visited=Set(), remaining=Set(graph))
   local_visited
 end
 
+#= Checks if there's Euler cycle in the graph by investigating
+   connectivity condition and evaluating if every vertex has even degree =#
 function check_euler()
   if length(partition()) == 1
     return all(map(v -> iseven(length(v.neighbors)), graph))
@@ -111,25 +131,28 @@ function check_euler()
     "Graph is not connected"
 end
 
-
+#= Returns text representation of the graph consisiting of each node's value
+   text and number of its neighbors. =#
 function graph_to_str()
   graph_str = ""
   for v in graph
     graph_str *= "****\n"
-    graph_str *= node_to_str(v.value)
-    graph_str *= "Neighbors $(length(v.neighbors)):\n"
+
+    n = v.value
+    if isa(n, Person)
+      node_str = "Person: $(n.name)\n"
+    else isa(n, Address)
+      node_str = "Street nr: $(n.streetNumber)\n"
+    end
+
+    graph_str *= node_str
+    graph_str *= "Neighbors: $(length(v.neighbors))\n"
   end
   graph_str
 end
 
-function node_to_str(n)
-  if isa(n, Person)
-    "Person: $(n.name)\n"
-  else isa(n, Address)
-    "Street nr: $(n.streetNumber)\n"
-  end
-end
-
+#= Tests graph functions by creating 100 graphs, checking Euler cycle
+  and creating text representation. =#
 function test_graph()
   for i=1:100
     global graph = GraphVertex[]
